a7e475335457e8c0bb1c96cf39ab09d5288a63fe
Hikari Ishihara <e205217@ie.u-ryukyu.ac.jp>
2021-12-09 22:10:22 +0900

    Step3

--- app/src/main/java/jp/ac/uryukyu/ie/e205217/report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205217/report5.java
@@ -2,12 +2,24 @@ package jp.ac.uryukyu.ie.e205217;
 
 public class report5 {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
+        String str = "百二十三";
+            
+        try {
+            int value = Integer.parseInt(str);  
+        }catch(NumberFormatException e){
             System.out.println("例外発生中");
-            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("処理終了");
         }
-    }
+        }
 }
+
+        //try{
+          //  String str = null;
+            //System.out.println(str.length());
+        //}catch(NullPointerException e){
+          //  System.out.println("例外発生中");
+            //System.out.println(e.getMessage());
+        //}
+    //}
+//}
